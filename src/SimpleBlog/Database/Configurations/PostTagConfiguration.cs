a472f74067ba7e17b5e0a8ee4f5c393a88bbda2c
Vlad Kuzmin <kuzmin.v2d@gmail.com>
2023-02-09 21:42:44 +0300

    fix database entities configurations

--- src/SimpleBlog/Database/Configurations/PostTagConfiguration.cs
+++ src/SimpleBlog/Database/Configurations/PostTagConfiguration.cs
@@ -12,6 +12,6 @@ public class PostTagConfiguration : IEntityTypeConfiguration<PostTag>
         builder.ToTable(TableName);
         builder.HasKey(p => p.Id);
 
-        builder.Property(p => p.Title).HasMaxLength(20).IsRequired();
+        builder.Property(p => p.Title).IsRequired();
     }
 }
--- src/SimpleBlog/Database/Configurations/ProfileConfiguration.cs
+++ src/SimpleBlog/Database/Configurations/ProfileConfiguration.cs
@@ -13,8 +13,6 @@ public class ProfileConfiguration : IEntityTypeConfiguration<Profile>
         builder.HasKey(p => p.Id);
 
         builder.Property(p => p.Name).IsRequired();
-
-        //builder.HasOne(p => p.Account).WithOne(a => a.Profile).HasForeignKey<Profile>(p => p.AccountId).IsRequired();
-
+        builder.Property(p => p.CreatedOn).IsRequired();
     }
 }
