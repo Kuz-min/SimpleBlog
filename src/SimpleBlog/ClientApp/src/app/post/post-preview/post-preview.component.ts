12e7875879f55842faf9fbba4cdbd09ef31529a1
Vlad Kuzmin <kuzmin.v2d@gmail.com>
2023-03-04 21:41:07 +0300

    update post preview component

--- src/SimpleBlog/ClientApp/src/app/post/post-preview/post-preview.component.ts
+++ src/SimpleBlog/ClientApp/src/app/post/post-preview/post-preview.component.ts
@@ -1,22 +1,15 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { Post, PostService } from 'simple-blog/core';
-
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Post } from 'simple-blog/core';
 
 @Component({
   selector: 'post-preview',
   templateUrl: './post-preview.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PostPreviewComponent implements OnInit {
+export class PostPreviewComponent {
 
-  @Input() postId: (number | null | undefined);
   @Input() post: (Post | null | undefined);
 
-  constructor(
-    private readonly _postService: PostService,
-  ) { }
-
-  ngOnInit(): void {
-  }
+  constructor() { }
 
 }
