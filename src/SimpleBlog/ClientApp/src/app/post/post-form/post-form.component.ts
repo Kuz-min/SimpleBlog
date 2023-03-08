db67f8edaf9f30c12868a7d5ab2d676711c3f481
Vlad Kuzmin <kuzmin.v2d@gmail.com>
2023-03-08 23:31:56 +0300

    update post form

--- src/SimpleBlog/ClientApp/src/app/post/post-form/post-form.component.ts
+++ src/SimpleBlog/ClientApp/src/app/post/post-form/post-form.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { catchError, filter, Observable, of, throwError } from 'rxjs';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, catchError, filter, first, Observable, of, Subscription, throwError } from 'rxjs';
 import { Post, PostTag, PostTagService } from 'simple-blog/core';
 
 @Component({
@@ -9,17 +9,19 @@ import { Post, PostTag, PostTagService } from 'simple-blog/core';
   templateUrl: './post-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PostFormComponent implements OnInit {
+export class PostFormComponent implements OnInit, OnDestroy {
 
   @Input() post: (Observable<Post | null> | null | undefined);
   @Input() disabled: (Observable<boolean> | null | undefined);
-  @Output() readonly complited = new EventEmitter<{ title: string, content: string, tagIds?: number[] }>();
+  @Output() readonly complited = new EventEmitter<{ title: string, content: string, image?: File, tagIds?: number[] }>();
 
   tags: (Observable<PostTag[] | null> | null) = null;
 
+  readonly imagePath = new BehaviorSubject<string | null>(null);
+
   readonly form = new FormGroup({
-    title: new FormControl(''),
-    content: new FormControl(''),
+    title: new FormControl('', [Validators.required]),
+    content: new FormControl('', [Validators.required]),
     tagIds: new FormControl<number[]>([]),
   });
 
@@ -33,24 +35,47 @@ export class PostFormComponent implements OnInit {
     );
 
     if (this.post) {
-      this.post.pipe(
+      this._postSubscription = this.post.pipe(
         filter(post => Boolean(post)),
+        first(),
       ).subscribe(
         next => this.form.patchValue(next!),
       );
     }
 
     if (this.disabled) {
-      this.disabled.subscribe(
+      this._disabledSubscription = this.disabled.subscribe(
         next => next ? this.form.disable() : this.form.enable(),
       );
     }
 
   }
 
-  onSubmit(): void {
-    let data = this.form.value;
-    this.complited.emit({ title: data.title!, content: data.content!, tagIds: data.tagIds! });
+  ngOnDestroy(): void {
+    this._postSubscription?.unsubscribe();
+    this._disabledSubscription?.unsubscribe();
   }
+
+  onSubmit(): void {
+    const data = this.form.value;
+    this.complited.emit({ title: data.title!, content: data.content!, image: this._imageFile!, tagIds: data.tagIds! });
+  }
+
+  imagePreview(event: any): void {
+    const file = (event.target as HTMLInputElement)?.files?.[0];
+
+    if (!file)
+      return;
+
+    this._imageFile = file;
+
+    const reader = new FileReader();
+    reader.onload = () => this.imagePath.next(reader.result as string);
+    reader.readAsDataURL(file);
+  }
+
+  private _imageFile: (File | null) = null;
+  private _postSubscription: (Subscription | null) = null;
+  private _disabledSubscription: (Subscription | null) = null;
 
 }
