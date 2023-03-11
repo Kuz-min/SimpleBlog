b404f1ea63d9b5b4d633fe0d7dd880ccd4903a5e
Vlad Kuzmin <kuzmin.v2d@gmail.com>
2023-03-11 23:58:31 +0300

    update post creator component

--- src/SimpleBlog/ClientApp/src/app/post/post-creator/post-creator.component.ts
+++ src/SimpleBlog/ClientApp/src/app/post/post-creator/post-creator.component.ts
@@ -1,36 +1,35 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject, catchError, finalize, first, switchMap, throwError } from 'rxjs';
 import { PostService } from 'simple-blog/core';
+import { PostChangedAlertComponent } from '../post-changed-alert/post-changed-alert.component';
 
 @Component({
   selector: 'post-creator',
   templateUrl: './post-creator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PostCreatorComponent implements OnInit {
+export class PostCreatorComponent {
 
-  lockForm = new BehaviorSubject<boolean>(false);
-  isCreated = new BehaviorSubject<boolean>(false);
+  readonly lockForm = new BehaviorSubject<boolean>(false);
 
   constructor(
+    private readonly _snackBar: MatSnackBar,
     private readonly _postService: PostService,
   ) { }
 
-  ngOnInit(): void {
-  }
-
-  onComplited(data: { title: string, content: string, tagIds?: number[] }): void {
+  onComplited(data: { title: string, content: string, image?: File, tagIds?: number[] }): void {
     this.lockForm.next(true);
-    this._postService.createAsync({ title: data.title, content: data.content, tagIds: data.tagIds }).subscribe(
-      next => {
-        this.lockForm.next(false);
-        this.isCreated.next(true);
-      },
-      error => {
-        console.error(error);
-        this.lockForm.next(false);
-      },
-    );
+    this._postService.createAsync(data).pipe(
+      first(),
+      switchMap(post => this._snackBar.openFromComponent(PostChangedAlertComponent, { verticalPosition: 'top', data: { message: 'Post created!', post: post } }).afterDismissed()),
+      catchError(error => error instanceof HttpErrorResponse ?
+        this._snackBar.openFromComponent(PostChangedAlertComponent, { verticalPosition: 'top', data: { message: `Error! ${error.message}` } }).afterDismissed()
+        : throwError(error)
+      ),
+      finalize(() => this.lockForm.next(false)),
+    ).subscribe();
   }
 
 }
