453c9332bd1c48803c5c06630389e791ef384f5c
Vlad Kuzmin <kuzmin.v2d@gmail.com>
2023-03-12 21:05:15 +0300

    update post editor component

--- src/SimpleBlog/ClientApp/src/app/post/post-editor/post-editor.component.ts
+++ src/SimpleBlog/ClientApp/src/app/post/post-editor/post-editor.component.ts
@@ -1,8 +1,10 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatSnackBar, MatSnackBarDismiss } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
-import { BehaviorSubject, catchError, filter, map, mergeMap, Observable, of, tap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, filter, finalize, first, map, Observable, of, switchMap, tap, throwError } from 'rxjs';
 import { Post, PostService } from 'simple-blog/core';
+import { PostChangedAlertComponent } from '../post-changed-alert/post-changed-alert.component';
 
 @Component({
   selector: 'post-editor',
@@ -12,45 +14,55 @@ import { Post, PostService } from 'simple-blog/core';
 export class PostEditorComponent implements OnInit {
 
   post: (Observable<Post | null> | null) = null;
-  lockForm = new BehaviorSubject<boolean>(false);
-  isUpdated = new BehaviorSubject<boolean>(false);
-  isDeleted = new BehaviorSubject<boolean>(false);
+
+  readonly lockForm = new BehaviorSubject<boolean>(false);
 
   constructor(
     private readonly _route: ActivatedRoute,
+    private readonly _snackBar: MatSnackBar,
     private readonly _postService: PostService,
   ) { }
 
   ngOnInit(): void {
     this.post = this._route.params.pipe(
-      filter(params => params['id']),
+      filter(params => Boolean(params['id'])),
       map(params => Number(params['id'])),
       tap(id => this._postId = id),
-      mergeMap(id => this._postService.getByIdAsync(id).pipe(
-        catchError(error => (error as HttpErrorResponse)?.status == 404 ? of(null) : throwError(error)),
+      switchMap(id => this._postService.getByIdAsync(id).pipe(
+        first(),
+        catchError(error => error instanceof HttpErrorResponse && error.status == 404 ? of(null) : throwError(error)),
       )),
     );
   }
 
-  updatePost(data: { title: string, content: string, tagIds?: number[] }): void {
+  updatePost(data: { title: string, content: string, image?: File, tagIds?: number[] }): void {
+    if (!this._postId)
+      return;
+
     this.lockForm.next(true);
-    this._postService.updateAsync(this._postId!, { title: data.title, content: data.content, tagIds: data.tagIds }).subscribe(
-      next => {
-        this.lockForm.next(false);
-        this.isUpdated.next(true);
-      },
-      error => {
-        this.lockForm.next(false);
-      },
-    );
+    this._postService.updateAsync(this._postId, data).pipe(
+      first(),
+      switchMap(post => this.showAlert('Post updated!', post)),
+      catchError(error => error instanceof HttpErrorResponse ? this.showAlert(`Error! ${error.message}`) : throwError(error)),
+      finalize(() => this.lockForm.next(false)),
+    ).subscribe();
   }
 
   deletePost(): void {
-    this._postService.deleteAsync(this._postId!).subscribe(
-      next => {
-        this.isDeleted.next(true);
-      },
-    );
+    if (!this._postId)
+      return;
+
+    this.lockForm.next(true);
+    this._postService.deleteAsync(this._postId).pipe(
+      first(),
+      switchMap(() => this.showAlert('Post deleted!')),
+      catchError(error => error instanceof HttpErrorResponse ? this.showAlert(`Error! ${error.message}`) : throwError(error)),
+      finalize(() => this.lockForm.next(false)),
+    ).subscribe();
+  }
+
+  private showAlert(message: string, post?: Post): Observable<MatSnackBarDismiss> {
+    return this._snackBar.openFromComponent(PostChangedAlertComponent, { verticalPosition: 'top', data: { message: message, post: post } }).afterDismissed();
   }
 
   private _postId: (number | null) = null;
